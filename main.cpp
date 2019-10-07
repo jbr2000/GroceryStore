abdf87d4869d425c38f55e5759321055b92c5b74
jbr2000 <jbrupert@mail.csuchico.edu>
2019-10-07 16:33:15 -0700

    can make a list of inputs

--- main.cpp
+++ main.cpp
@@ -8,14 +8,27 @@ int main()
   int numItems = 0;
   char input;
 do 
-{ 
+  { 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-}
+  cin>>input; 
+  if(input=='a' || input =='A')
+    {
+    if (numItems>4)
+      {
+       cout<<"You'll need a bigger list!";
+      }
+    else
+      {
+    cout<<"What is the item?\n";
+    cin>>list[numItems];
+    numItems++;
+      }
+    }
+  }
 while(input != 'q' &&  input != 'Q');
 
 
